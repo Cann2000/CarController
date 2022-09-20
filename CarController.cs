50ed78564258a3ea2be71d55be9ecb8af684b1f0
Muhammet Can Yıldız <94134588+Cann2000@users.noreply.github.com>
2022-09-20 18:02:52 +0300

    GameCod

--- CarController.cs
+++ CarController.cs
@@ -7,99 +7,54 @@ public class CarController : MonoBehaviour
     public float move;
     public float angle;
 
-    public GameObject Karakter;
-
-    public bool ArabayaBinildi;
-
-    [SerializeField] OyunAyarlarý oyunayarlarý;
 
     [SerializeField] float MotorForce;
     [SerializeField] float BreakForce;
     [SerializeField] float SteerAngle;
 
-    [SerializeField] Transform SolOnWhell;
-    [SerializeField] Transform SagOnWhell;
-    [SerializeField] Transform SolArkaWhell;
-    [SerializeField] Transform SagArkaWhell;
+    [SerializeField] Transform LeftFrontWhell;
+    [SerializeField] Transform RightFronWhell;
+    [SerializeField] Transform LeftRearWhell;
+    [SerializeField] Transform RightRearWhell;
 
     [SerializeField] WheelCollider[] Wheels;
 
-    public GameObject centerofmass;
-    public Rigidbody rb;
-    void Start()
-    {
-        Karakter = GameObject.FindGameObjectWithTag("Player");
-        oyunayarlarý = GameObject.FindGameObjectWithTag("Player").GetComponent<OyunAyarlarý>();
-        rb.centerOfMass = centerofmass.transform.localPosition;
-    }
 
     private void Update()
     {
-        Arabadanin();
+        CarController();
+        UpdateWheels();
+
     }
 
-    private void FixedUpdate()
+
+    void CarController()
     {
-        if (ArabayaBinildi)
-        {
-            move = Input.GetAxis("Vertical");
-            angle = Input.GetAxis("Horizontal");
+        move = Input.GetAxis("Vertical");
+        angle = Input.GetAxis("Horizontal");
 
-            foreach (var wheel in Wheels)
-            {
-                if (move <= 0 || move >= 0)
-                {
-                    wheel.brakeTorque = 0;
-                    wheel.motorTorque = move * MotorForce;
-                }
-                if (move == 0)
-                {
-                    wheel.brakeTorque = BreakForce;
-                }
-            }
-            for (int i = 0; i < Wheels.Length; i++)
-            {
-                if (i < 2)
-                {
-                    Wheels[i].steerAngle = Input.GetAxis("Horizontal") * SteerAngle;
-                }
-            }
-        }
-
-        if (!ArabayaBinildi)
+        foreach (var wheel in Wheels)
         {
-            foreach (var wheel in Wheels)
+            if (move <= 0 || move >= 0)
+            {
+                wheel.brakeTorque = 0;
+                wheel.motorTorque = move * MotorForce;
+            }
+            if (move == 0)
             {
                 wheel.brakeTorque = BreakForce;
             }
         }
-
-        UpdateWheels();
-    }
-
-    void Arabadanin()
-    {
-        if (ArabayaBinildi)
+        for (int i = 0; i < Wheels.Length; i++)
         {
-            if (Input.GetKeyDown(KeyCode.E))
+            if (i < 2)
             {
-                    hit.transform.parent.GetChild(0).GetComponent<Camera>().enabled = true;
-                    //hit.transform.GetChild(0).GetComponent<Camera>().enabled = true;
-                    gameObject.transform.SetParent(hit.transform.parent);
-                    hit.transform.parent.GetComponent<CarController>().ArabayaBinildi = true;
-                    //hit.transform.GetComponent<CarController>().ArabayaBinildi = true;
-                    //hit.transform.GetComponent<Rigidbody>().mass = 1000;
-                    hit.transform.parent.GetComponent<Rigidbody>().mass = 1000;
-                    gameObject.SetActive(false);
-                for (int i = 0; i < oyunayarlarý.ArabadaKapanýcakPaneller.Length; i++)
-                {
-                    oyunayarlarý.ArabadaKapanýcakPaneller[i].SetActive(true);
-                }
+                Wheels[i].steerAngle = Input.GetAxis("Horizontal") * SteerAngle;
             }
         }
     }
 
-    //void fren()
+    //void Breake()
     //{
     //    Wheels[0].brakeTorque = BreakForce;
     //    Wheels[1].brakeTorque = BreakForce;
@@ -109,10 +64,10 @@ public class CarController : MonoBehaviour
 
     void UpdateWheels()
     {
-        UpdateTurnWheels(Wheels[0], SolOnWhell);
-        UpdateTurnWheels(Wheels[1], SagOnWhell);
-        UpdateTurnWheels(Wheels[2], SolArkaWhell);
-        UpdateTurnWheels(Wheels[3], SagArkaWhell);
+        UpdateTurnWheels(Wheels[0], LeftFrontWhell);
+        UpdateTurnWheels(Wheels[1], RightFronWhell);
+        UpdateTurnWheels(Wheels[2], LeftRearWhell);
+        UpdateTurnWheels(Wheels[3], RightRearWhell);
     }
     void UpdateTurnWheels(WheelCollider WheellCollider, Transform WhellTransform)
     {
